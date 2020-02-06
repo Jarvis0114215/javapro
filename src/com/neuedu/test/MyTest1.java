620aff12a3c21d4ceba838c4c988095b732d821b
jiaxiaowei1997 <1272182012@qq.com>
2020-02-06 16:55:39 +0800

    新

--- src/com/neuedu/test/MyTest1.java
+++ src/com/neuedu/test/MyTest1.java
@@ -5,6 +5,6 @@ package com.neuedu.test;
  */
 public class MyTest1 {
     public static void main(String[] args) {
-        System.out.println("新建项目");
+        System.out.println("新建项目1");
     }
 }
--- src/com/neuedu/test/MyTest2.java
+++ src/com/neuedu/test/MyTest2.java
@@ -5,6 +5,6 @@ package com.neuedu.test;
  */
 public class MyTest2 {
     public static void main(String[] args) {
-        System.out.println("111");
+        System.out.println("1111");
     }
 }
