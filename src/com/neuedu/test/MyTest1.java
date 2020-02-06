fc80827c0ff4db85ef934e14d28e0e677e9c8033
jiaxiaowei1997 <1272182012@qq.com>
2020-02-06 16:38:37 +0800

    1111

---
+++ src/com/neuedu/test/MyTest1.java
@@ -0,0 +1,10 @@
+package com.neuedu.test;
+
+/**
+ * Create by Jia on 2020/2/6/006 15:38
+ */
+public class MyTest1 {
+    public static void main(String[] args) {
+        System.out.println("新建项目");
+    }
+}
