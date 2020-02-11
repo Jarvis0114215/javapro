9ef00600b82fde3c87e51defbc11532331a2d4e6
jiaxiaowei1997 <1272182012@qq.com>
2020-02-11 19:37:59 +0800

    gengxin

---
+++ src/com/Neuedu/test/MyTest.java
@@ -0,0 +1,13 @@
+package com.Neuedu.pr;
+
+/**
+ * Create by Jia on 2020/2/5/005 14:23
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        p1.setAge(10);
+        System.out.println(p1.getAge());
+        p1.run();
+    }
+}
